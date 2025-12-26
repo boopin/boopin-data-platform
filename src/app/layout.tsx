fb54e602313f39aae8d4307a3ace8545ffbe4473
Boopin <36192860+boopin@users.noreply.github.com>
2025-12-26 18:09:50 +0400

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "Boopin Data Platform",
+  title: "Boopin Data Platform TEST 123",
   description: "1st Party Data Collection & Analytics",
 };
 
